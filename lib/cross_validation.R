289209286eb505a2fdb4c92231278d7cf8e14d9a
YuqLiu <60188955+YuqLiu@users.noreply.github.com>
2020-04-14 01:47:17 -0400

    Update cross_validation.R

--- lib/cross_validation.R
+++ lib/cross_validation.R
@@ -5,7 +5,7 @@
 ### Author: Chengliang Tang
 ### Project 3
 
-cv.function <- function(dat_train, K, f, lambda){
+cv.functionYQ <- function(dat_train, K, f, lambda, maxIter){
   ### Input:
   ### - train data frame
   ### - K: a number stands for K-fold CV
@@ -15,19 +15,17 @@ cv.function <- function(dat_train, K, f, lambda){
   n.fold <- round(n/K, 0)
   set.seed(0)
   s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
-  train_rmse <- matrix(NA, ncol = 10,nrow = K)
-  test_rmse <- matrix(NA, ncol = 10, nrow = K)
+  train_rmse <- matrix(NA, ncol = (maxIter%/%3), nrow = K)
+  test_rmse <- matrix(NA, ncol = (maxIter%/%3), nrow = K)
   
   for (i in 1:K){
     train.data <- dat_train[s != i,]
     test.data <- dat_train[s == i,]
     
-    result <- gradesc(f = f, lambda =lambda,
-                   lrate = 0.01, max.iter = 100, stopping.deriv = 0.01,
-                               data = dat_train, train = train.data, test = test.data)
+    result <- ALS(data, train, test, f = f, maxIters = maxIter, lambda = lambda)
   
-    train_rmse[i,] <-  result$train_rmse
-    test_rmse[i,] <-   result$test_rmse
+    train_rmse[i,] <-  result$TrainRMSE
+    test_rmse[i,] <-   result$TestRMSE
     
   }		
   return(list(mean_train_rmse = apply(train_rmse, 2, mean), mean_test_rmse = apply(test_rmse, 2, mean),
